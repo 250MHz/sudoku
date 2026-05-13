78b91a28144e666c96aa452a14f6cf9acf0135a2
250MHz <95242017+250MHz@users.noreply.github.com>
2026-05-13 01:43:51 -0700

    problem: Match paper's fitness function
    Elite population learning uses the fitness value when calculating the
    probability for whether an individual should be replaced with a copy of
    an elite. By calculating it differently, we could be messing with that
    probabiliy which is giving us the shit results.

--- src/sudoku/problem.py
+++ src/sudoku/problem.py
@@ -1,5 +1,40 @@
 import numpy as np
+from numba import njit
 from pymoo.core.problem import Problem
+
+
+@njit
+def evaluate_population_binary(X_pop, N, sqrt_N):
+    pop_size = X_pop.shape[0]
+    out = np.zeros((pop_size, 1))
+
+    for i in range(pop_size):
+        grid = X_pop[i].reshape((N, N))
+        fitness = 0
+        for c in range(N):
+            seen = 0
+            unique_count = 0
+            for r in range(N):
+                val = grid[r, c]
+                if not (seen & (1 << val)):
+                    seen |= 1 << val
+                    unique_count += 1
+            if unique_count < N:
+                fitness += 1
+        for br in range(sqrt_N):
+            for bc in range(sqrt_N):
+                seen = 0
+                unique_count = 0
+                for r in range(br * sqrt_N, (br + 1) * sqrt_N):
+                    for c in range(bc * sqrt_N, (bc + 1) * sqrt_N):
+                        val = grid[r, c]
+                        if not (seen & (1 << val)):
+                            seen |= 1 << val
+                            unique_count += 1
+                if unique_count < N:
+                    fitness += 1
+        out[i, 0] = fitness
+    return out
 
 
 class SudokuProblem(Problem):
@@ -21,34 +56,4 @@ class SudokuProblem(Problem):
         super().__init__(n_var=self.N**2, n_obj=1, xl=1, xu=self.N, vtype=np.int32)
 
     def _evaluate(self, x: np.ndarray, out: dict, *args, **kwargs):
-        pop_size = len(x)
-        major_matrices = x.reshape((pop_size, self.N, self.N))
-        col_conflicts = np.zeros(pop_size, dtype=np.int32)
-        subblock_conflicts = np.zeros(pop_size, dtype=np.int32)
-
-        # This fitness function is different than Wang et al.'s
-        # We count the number of values that are missing from
-        # the columns and sub-blocks,
-        # rather than doing a binary value for whether constraint is met.
-
-        for i in range(self.N):
-            columns = major_matrices[:, :, i]
-            # In every column i, check if a value in [1, N] is missing.
-            # If so, increment num violations for that individual.
-            for val in range(1, self.N + 1):
-                is_missing = ~np.any(columns == val, axis=1)
-                col_conflicts += is_missing.astype(np.int32)
-
-        for r_offset in range(0, self.N, self.sqrt_N):
-            for c_offset in range(0, self.N, self.sqrt_N):
-                subblocks = major_matrices[
-                    :,
-                    r_offset : r_offset + self.sqrt_N,
-                    c_offset : c_offset + self.sqrt_N,
-                ]
-                subblocks_flat = subblocks.reshape((pop_size, self.N))
-                for val in range(1, self.N + 1):
-                    is_missing = ~np.any(subblocks_flat == val, axis=1)
-                    subblock_conflicts += is_missing.astype(np.int32)
-
-        out["F"] = (col_conflicts + subblock_conflicts).reshape(-1, 1).astype(float)
+        out["F"] = evaluate_population_binary(x, self.N, self.sqrt_N)
